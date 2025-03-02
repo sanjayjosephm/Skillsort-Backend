fe19c9409e85cddb8f39b0fcd2b9fb8634bfcdfc
Sanjay Joseph <sanjayjosephm@gmail.com>
2025-03-02 14:17:33 +0530

    port addition

--- app.py
+++ app.py
@@ -92,5 +92,6 @@ def welcomePage():
     test = "The server is up and running"
     return test
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Get port from environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
